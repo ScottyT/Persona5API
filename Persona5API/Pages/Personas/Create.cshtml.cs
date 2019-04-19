0e0bf5386287fd073e7b346876e22f58afda3620
Scott Tucker <stuck04@gmail.com>
2019-04-19 13:24:02 -0400

    Created razor pages version of the personas list.

---
+++ Persona5API/Pages/Personas/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Persona5API.Data;
+using Persona5API.Models;
+
+namespace Persona5API.Pages.Personas
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Persona5API.Data.ApplicationDbContext _context;
+
+        public CreateModel(Persona5API.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Persona Persona { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Personas.Add(Persona);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
