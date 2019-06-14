1526933e943b5e50b1aa6fbd7a33a2f5092ca122
Scott Tucker <stuck04@gmail.com>
2019-06-14 17:10:41 -0400

    more css changes

--- Persona5API/wwwroot/js/persona.main.js
+++ Persona5API/wwwroot/js/persona.main.js
@@ -57,7 +57,14 @@ persona.main = (function ($) {
 
 							})
 					}
-				}				
+                },
+                filters: {
+                    uppercase: function (value) {
+                        if (!value) return ''
+                        value = value.toString()
+                        return value.charAt(0).toUpperCase() + value.slice(1)
+                    }
+                }
 			});
 		}
 	};
